a2f2e217298f8d6a12c04d2a270fbefac1c17865
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2024-02-28 20:02:14 -0800

    Enable matcher support

--- src/middleware/TemplateMiddleware.php
+++ src/middleware/TemplateMiddleware.php
@@ -2,19 +2,24 @@
 
 namespace Hiraeth\Templates;
 
+use Dotink\Jin;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\StreamFactoryInterface as StreamFactory;
-use Psr\Http\Message\ResponseFactoryInterface as ResponseFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use RuntimeException;
 
 /**
  * {@inheritDoc}
  */
 class TemplateMiddleware implements Middleware
 {
+	/**
+	 * @var Jin\Parser
+	 */
+	protected $jin;
+
 	/**
 	 * The template manager
 	 *
@@ -34,8 +39,9 @@ class TemplateMiddleware implements Middleware
 	/**
 	 * Create a new instance of the middleware
 	 */
-	public function __construct(Manager $manager, StreamFactory $stream_factory)
+	public function __construct(Jin\Parser $jin, Manager $manager, StreamFactory $stream_factory)
 	{
+		$this->jin           = $jin;
 		$this->manager       = $manager;
 		$this->streamFactory = $stream_factory;
 	}
@@ -49,8 +55,9 @@ class TemplateMiddleware implements Middleware
 		$response = $handler->handle($request);
 
 		if ($response->getStatusCode() == 404) {
-			$uri_path  = $request->getUri()->getPath();
-			$is_dir    = substr($uri_path, -1, 1) == '/';
+			$matchers = array();
+			$uri_path = $request->getUri()->getPath();
+			$is_dir   = substr($uri_path, -1, 1) == '/';
 
 			if ($is_dir) {
 				$path = '@pages' . $uri_path . 'index.html';
@@ -61,7 +68,7 @@ class TemplateMiddleware implements Middleware
 				$alt  = '@pages' . $uri_path . '/index.html';
 			}
 
-			if ($request->getHeaderLine('HX-Request')) {
+			if ($request->getHeaderLine('X-Requested-With') == 'xmlhttprequest') {
 				$path = str_replace('/' . basename($path), '/%' . basename($path), $path);
 				$alt  = str_replace('/' . basename($alt),  '/%' . basename($alt),  $alt);
 			} else {
@@ -69,31 +76,82 @@ class TemplateMiddleware implements Middleware
 				$alt  = str_replace('/' . basename($alt),  '/@' . basename($alt),  $alt);
 			}
 
-			if ($this->manager->has($path)) {
-				if ($is_dir || basename($path) != '@index.html') {
-					$response = $response
-						->withStatus(200)
-						->withHeader('Content-Type', 'text/html; charset=utf-8')
-						->withBody($this->streamFactory->createStream(
-							$this->manager->load($path, ['request' => $request])->render()
-						))
-					;
-				}
+			if ($is_dir) {
+				$matcher_config = $path . '~matchers.jin';
+			} else {
+				$matcher_config = dirname($path) . '/' . '~matchers.jin';
+			}
 
-			} elseif ($this->manager->has($alt)) {
-				$response = $response->withStatus(301);
+			if ($this->manager->has($matcher_config)) {
+				$matchers = $this->jin->parse($this->manager->load($matcher_config)->render());
+				$endpoint = basename($uri_path);
+				$matches  = array();
+			}
 
-				if ($is_dir) {
-					$response = $response->withHeader(
-						'Location',
-						(string) $request->getUri()->withPath(substr($uri_path, 0, -1))
-					);
+			if (!$matchers || !isset($matchers[$endpoint])) {
+				if ($this->manager->has($path)) {
+					if ($is_dir || basename($path) != '@index.html') {
+						$response = $response
+							->withStatus(200)
+							->withHeader('Content-Type', 'text/html; charset=utf-8')
+							->withBody($this->streamFactory->createStream(
+								$this->manager->load($path, ['request' => $request])->render()
+							))
+						;
+					}
+
+				} elseif ($this->manager->has($alt)) {
+					$response = $response->withStatus(301);
+
+					if ($is_dir) {
+						$response = $response->withHeader(
+							'Location',
+							(string) $request->getUri()->withPath(substr($uri_path, 0, -1))
+						);
+
+					} else {
+						$response = $response->withHeader(
+							'Location',
+							(string) $request->getUri()->withPath($uri_path . '/')
+						);
+					}
 
 				} else {
-					$response = $response->withHeader(
-						'Location',
-						(string) $request->getUri()->withPath($uri_path . '/')
-					);
+					foreach ($matchers as $template => $matcher) {
+						if (!preg_match('#' . $matcher['pattern'] . '#', $endpoint, $matches)) {
+							continue;
+						}
+
+						array_shift($matches);
+
+						if (count($matches) != count($matcher['mapping'])) {
+							throw new RuntimeException(sprintf(
+								''
+							));
+						}
+
+						$matches  = array_combine($matcher['mapping'], $matches);
+
+						if ($request->getHeaderLine('X-Requested-With') == 'xmlhttprequest') {
+							$template = '%' . $template . '.html';
+						} else {
+							$template = '@' . $template . '.html';
+						}
+
+						$response = $response
+							->withStatus(200)
+							->withHeader('Content-Type', 'text/html; charset=utf-8')
+							->withBody($this->streamFactory->createStream(
+								$this->manager->load(
+									'@pages' . dirname($uri_path) . '/' . $template,
+									[
+										'request'    => $request,
+										'parameters' => $matches
+									]
+								)->render()
+							))
+						;
+					}
 				}
 			}
 		}
